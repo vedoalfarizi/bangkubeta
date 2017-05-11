1c8e27ab843d58b7f5b21b5de2253e29ba5e9d43
dartika anie marian <dartika anie marian>
2017-05-11 11:35:51 +0700

    inedx login

--- header.php
+++ header.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css" src="stlye.css"></style>
+</head>
+<body>
+
 <div id="header">
 	<ul>
 		<li><img src=""></li>
@@ -5,4 +13,13 @@
 		<li><a href="profil_org.php">Profil</a></li>
 		<li><a href="proses.php?log=logout">Keluar</a></li>
 	</ul>
+
+	<ul>
+   <li class="active"><a href="#"><span>Home</span></a></li>
+   <li><a href="#"><span>Products</span></a></li>
+   <li><a href="#"><span>Company</span></a></li>
+   <li class="last"><a href="#"><span>Contact</span></a></li>
+</ul>
 </div>
+</body>
+</html>
---
+++ organisasi/kegiatan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bangku</title>
+</head>
+<body>
+	<div>
+		<form action="proses.php" method="POST">
+			<label>Nama Kegiatan</label>
+			<input type="text" name="n_keg"><br>
+			<label>Deskripsi</label>
+			<input type="text" name="desk"><br>
+			<label>KIni dia</label>
+			<input type="checkbox" name="kat" value="fiksi">Fiksi
+			<input type="checkbox" name="kat" value="nonfiksi">Non Fiksi
+			<input type="checkbox" name="kat" value="nonfiksi">Lain-lain <br>	
+			<label>Dilaksanakan pada</label>
+			<input type="date" name="jadwal"><br>
+			<input type="submit" name="kegiatan" value="Bagikan">
+		</form>
+	</div>
+</body>
+</html>
